adb72ecc3072fa6afd5b194aa0a7556499b40dc7
kokhinmaungwin <231964401+kokhinmaungwin@users.noreply.github.com>
2026-01-03 02:30:32 +0630

    Save local changes before pull

--- convert.js
+++ convert.js
@@ -1,17 +1,16 @@
 const fs = require('fs');
 const path = require('path');
-const Jimp = require('jimp');
-const pngToIcoModule = require('png-to-ico');
+const Jimp = require('jimp').default;   
+const pngToIco = require('png-to-ico');
 
 async function convert() {
   try {
-    const src = path.join(__dirname, 'icon-48.png');
+    const src = path.join(__dirname, 'icon-48.png');  
     const img = await Jimp.read(src);
 
     const resized = await img.resize(256, 256).getBufferAsync(Jimp.MIME_PNG);
 
-    // Use .default for ES module compatibility
-    const icoBuffer = await pngToIcoModule.default(resized);
+    const icoBuffer = await pngToIco(resized);
 
     fs.writeFileSync('favicon.ico', icoBuffer);
     console.log('favicon.ico created!');
