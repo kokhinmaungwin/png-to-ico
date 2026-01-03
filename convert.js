fba3201e101d5a5083c89cf647c00ce913786eea
kokhinmaungwin <231964401+kokhinmaungwin@users.noreply.github.com>
2026-01-03 10:13:02 +0630

    Move PNG files to png folder and update paths

--- convert.js
+++ convert.js
@@ -5,7 +5,7 @@ const pngToIcoModule = require('png-to-ico');
 
 async function convert() {
   try {
-    const src = path.join(__dirname, 'icon-48.png');
+    const src = path.join(__dirname, 'png/icon-48.png');
     const img = await Jimp.read(src);
 
     const resized = await img.resize(256, 256).getBufferAsync(Jimp.MIME_PNG);
